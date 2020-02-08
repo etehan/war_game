092f4b76c05f34bce243cefdbec69c940f3c486e
Etehan vazquez <r3ggerjagg3r@gmail.com>
2020-02-08 16:12:32 -0500

    added these 2 files

---
+++ character_dialog.js
@@ -0,0 +1,4 @@
+var answer1 = prompt ("how was ur day?");
+console.log("you answered " + answer1)
+var answer2 = prompt("you look ugly");
+console.log("you angerly stated "+ answer2)
---
+++ testing.js
@@ -0,0 +1,21 @@
+var tank_hp = 1000;
+var machine_gun = 10;
+var army_man = 100;
+var german_tank_hp = 1000
+var german_machine_gun = 10;
+var german_army_man = 100;
+var mhm = 3;
+function army_man_shoots(){
+    console.log(army_man_shoots)
+    german_army_man = german_army_man - machine_gun
+    german_army_man = german_army_man - machine_gun
+    german_army_man = german_army_man - machine_gun
+    german_army_man = german_army_man - machine_gun
+    german_army_man = german_army_man - machine_gun
+    console.log(german_army_man
+        )
+}
+
+while (mhm<11){
+    mhm+1;
+}
